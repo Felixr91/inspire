9dffaaea231e1162f0c630fe0e766792f10acf4a
Felix <felixr91@gmail.com>
2018-11-19 09:33:39 -0700

    comit weather

--- app/components/weather/weather-service.js
+++ app/components/weather/weather-service.js
@@ -11,7 +11,6 @@ export default class WeatherService {
 
 	getWeather(drawWeather) {
 		console.log('Calling the Weatherman')
-<<<<<<< HEAD
 		weatherApi()
 			.then(function (res) {
 				localStorage.setItem('weather', JSON.stringify(res.data))
@@ -20,14 +19,5 @@ export default class WeatherService {
 				let kelvin = res.data.main.temp
 				drawWeather(Math.floor(1.8 * (kelvin - 273) + 32))
 			})
-=======
-		weatherApi().then(function (res) {
-			// HEY FUN FACT 
-			// Have you ever wanted to know the temperature measured in kelvin? That is what this data returns!
-			// res.data.temp is the temperature in Kelvin
-			// You should probably convert the temperature data to either F or C
-			callWhenDone(res.data);
-		})
->>>>>>> 92ae929374ac07ced5429c24c442f719c237328d
 	}
 }
